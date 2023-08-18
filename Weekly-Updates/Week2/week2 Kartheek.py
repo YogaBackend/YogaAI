7f8e72c302fc1d2e8179711d0eb421576146fee2
YogaBackend <142134423+YogaBackend@users.noreply.github.com>
2023-08-18 20:10:06 +0530

    Add files via upload

---
+++ Weekly-Updates/Week2/week2 Kartheek.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+
+def process(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 25)
+    img_canny = cv2.Canny(img_blur, 5, 50)
+    kernel = np.ones((3, 3))
+    img_dilate = cv2.dilate(img_canny, kernel, iterations=4)
+    img_erode = cv2.erode(img_dilate, kernel, iterations=1)
+    return img_erode
+
+def get_contours(img, img_original):
+    img_contours = img_original.copy()
+    contours, hierarchies = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    cv2.drawContours(img_contours, contours, -1, (0, 255, 0), -1) 
+    return img_contours
+
+# Use webcam (camera index 0)
+cap = cv2.VideoCapture(0)
+
+success, img1 = cap.read()
+if not success:
+    print("Error: Could not read the first frame from the webcam.")
+    exit()
+
+success, img2 = cap.read()
+heat_map = np.zeros(img1.shape[:-1])
+
+while success:
+    diff = cv2.absdiff(img1, img2)
+    img_contours = get_contours(process(diff), img1)
+
+    heat_map[np.all(img_contours == [0, 255, 0], 2)] += 3
+    heat_map[np.any(img_contours != [0, 255, 0], 2)] -= 3
+    heat_map[heat_map < 0] = 0
+    heat_map[heat_map > 255] = 255
+
+    # Find bounding box around the motion
+    non_zero_pixels = np.argwhere(heat_map > 0)
+    if len(non_zero_pixels) > 0:
+        x, y, w, h = cv2.boundingRect(non_zero_pixels)
+        
+        if w > 0 and h > 0:  # Check if the width and height are valid
+            # Crop motion heatmap
+            cropped_heatmap = heat_map[y:y+h, x:x+w]
+
+            # Crop video feed using the same region
+            cropped_frame = img2[y:y+h, x:x+w]
+
+            img_mapped = cv2.applyColorMap(cropped_heatmap.astype('uint8'), cv2.COLORMAP_JET)
+
+            cv2.imshow("Original", cropped_frame)
+            cv2.imshow("Heat Map", img_mapped)
+
+
+    img1 = img2
+    success, img2 = cap.read()
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Additional loop to keep the window open until 'q' is pressed
+while cv2.waitKey(0) != ord('q'):
+    pass
+
+cap.release()
+cv2.destroyAllWindows()
